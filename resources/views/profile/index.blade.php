6d768ba1639619f5a864c2e3036405533316cbe4
SoPhors <91106971+PhoSophors@users.noreply.github.com>
2023-07-28 21:03:29 +0700

    hello

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -15,15 +15,14 @@
 <body>
 
     @extends('layouts.app-master')
-    {{-- @extends('layouts.partials.navbar') --}}
     @section('content')
     <br/><br/><br/>
 
-    {{-- ======================================================================= --}}
+{{-- ======================================================================= --}}
 {{-- Section Profile User --}}
 {{-- ======================================================================= --}}
 <section class="UserProfile">
-    <div class="container py-5">
+    <div class="container-fluid py-5">
 
         <div class="row">
         <div class="col-lg-4">
@@ -96,7 +95,7 @@
     <!------ Include the above in your HEAD tag ---------->
 
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <div class="container w3-animate-opacity">
+    <div class="container-fluid w3-animate-opacity">
         <div class="tab">
             <button class="tablinks" onclick="openCity(event, 'IDphoto')">Photo</button>
             <button class="tablinks" onclick="openCity(event, 'IDplace')">Place</button>
@@ -105,62 +104,56 @@
         {{-- tap photo under profile --}}
         <div id="IDphoto" class="tabcontent">
         <table class="w3-animate-opacity">
-
-                {{-- @if ($posts->count() > 0)
-                        <ul>
+            <div>
+                @if ($posts->count() > 0)
+                    <div class="container-fluid mb-5">
+                        <div class="row gy-4 masonry">
                             @foreach ($posts as $post)
-                                <div class="card" style="width: 18rem;">
-                                    <img class="card-img-top" src="{{ $post->image }}" alt="Card image cap">
-                                    <div class="card-body">
-                                    <h5 hidden class="card-title">{{ $post->title }}</h5>
-                                    <p hidden class="card-text">{{ $post->description }}</p>
-                                    <p hidden class="card-text">{{ $post->location }}</p>
-                                    <a  href="{{ route('uploadphoto.show', $post->id) }}" class="btn btn-primary">show</a>
-                                    </div>
+                                <div class="col-lg-3 col-md-4 col-6">
+                                    <a href="{{ route('uploadphoto.show', $post->id) }}">
+                                        <img
+                                            style=
+                                            "
+                                            cursor: pointer;
+                                            width: 100%;
+                                            height: 100%;
+                                            max-height: 200px;
+                                            object-fit: cover;
+                                            "
+                                            src="{{ $post->image }} "
+                                            class="img-fluid"
+                                        >
 
+                                        <h5 hidden class="card-title">{{ $post->title }}</h5>
+                                        <p hidden class="card-text">{{ $post->description }}</p>
+                                        <p hidden class="card-text">{{ $post->location }}</p>
+                                        {{-- <a  href="{{ route('uploadphoto.show', $post->id) }}" class="btn btn-primary">show</a> --}}
+                                    </a>
                                 </div>
                             @endforeach
-                        </ul>
-                    @else
-                        <p>No posts found.</p>
-                    @endif --}}
-
-                    @if ($posts->count() > 0)
-                        <div class="container-fluid mb-5">
-                            <div class="row gy-4 masonry">
-                                @ @foreach ($posts as $post)
-                                <div class="col-lg-3 col-md-4 col-6">
-                                    <img src="{{ $post->image }}"  class="img-fluid">
-                                    <h5 hidden class="card-title">{{ $post->title }}</h5>
-                                    <p hidden class="card-text">{{ $post->description }}</p>
-                                    <p hidden class="card-text">{{ $post->location }}</p>
-                                    {{-- <a  href="{{ route('uploadphoto.show', $post->id) }}" class="btn btn-primary">show</a> --}}
-                                </div>
-                                @endforeach
-                            </div>
                         </div>
+                    </div>
+                @else
+                    <p>No posts found.</p>
+                @endif
+            </div>
 
-                    @else
-                        <p>No posts found.</p>
-                    @endif
-
-                    <script>
-                    window.onload = function () {
-                        var imgDefer = document.getElementsByTagName("img");
-                        for (var i = 0; i < imgDefer.length; i++) {
-                            if (imgDefer[i].getAttribute("data-src")) {
-                                imgDefer[i].setAttribute("src", imgDefer[i].getAttribute("data-src"));
-                            }
-                        }
-                        var $container = $(".masonry");
-                        $container.imagesLoaded(function () {
-                            $container.masonry({
-                                percentPosition: true
-                            });
-                        });
-                    };
-                    </script>
-
+            <script>
+            window.onload = function () {
+                var imgDefer = document.getElementsByTagName("img");
+                for (var i = 0; i < imgDefer.length; i++) {
+                    if (imgDefer[i].getAttribute("data-src")) {
+                        imgDefer[i].setAttribute("src", imgDefer[i].getAttribute("data-src"));
+                    }
+                }
+                var $container = $(".masonry");
+                $container.imagesLoaded(function () {
+                    $container.masonry({
+                        percentPosition: true
+                    });
+                });
+            };
+            </script>
         </table>
         </div>
         {{-- End tap photo under profile --}}
